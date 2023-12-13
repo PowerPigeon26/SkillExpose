f6318a19cf040b5bd2c9b12f98b260e66ca4a618
Maverick Grayer <maverick72032@gmail.com>
2023-12-13 16:26:22 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AbilityExpose.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
